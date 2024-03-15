715546e4533f730a604bbd4b0d7a67023b516921
AbsoluteZero000 <ahmedwaelwanas@gmail.com>
2024-03-15 16:59:22 +0200

    added high score feature

--- guessingGame/main.py
+++ guessingGame/main.py
@@ -2,11 +2,33 @@ import random
 
 
 mn, mx = 0, 10
-randomNumber = int(random.random()*(mx+1) - 0.000001)
+attempts_list = []
+while True:
+    attempts = 0
+    randomNumber = int(random.random()*(mx+1) - 0.000001)
+    print("Welcome to the Guessing Game!\n")
+    print(f"The current high score is {min(attempts_list)}" if len(attempts_list) > 0 else "There's no current high score")
+    while True:
+        userInput = int(input(f"Please enter you guess between {mn} and {mx}: "))
+        try:
+            if userInput < mn or userInput > mx:
+                raise ValueError("invalid input try again")
+        except ValueError as err:
+            print(err)
+        if randomNumber == userInput:
+            print("You guessed it Correctly!")
+            attempts_list.append(attempts)
+            print(f"attempts = {attempts}")
+            break
+        else:
+            print("You didn't guess it Correctly :(")
+            if userInput > randomNumber:
+                print("It's lower than the number you entered")
+            else:
+                print("It's higher than the number you entered")
+            attempts+=1
 
-userInput = int(input(f"Welcome to the Guessing Game!\nPlease enter you guess between {mn} and {mx}: "))
-if randomNumber == userInput:
-    print("You guessed it Correctly!")
-else:
-    print("You didn't guess it Correctly :(")
-    print(f"The Correct answer is {randomNumber}")
+
+    choice = input("Do you want to play again: ")
+    if not (choice == "yes" or choice == 'y'):
+        break
